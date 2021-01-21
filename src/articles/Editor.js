f032cf5d6870df581c591b8da58640126b88d2fd
Mason <1076536974@qq.com>
2021-01-21 23:58:03 +0800

    完成代码高亮的功能

--- src/articles/Editor.js
+++ src/articles/Editor.js
@@ -1,13 +1,16 @@
 import React from "react";
-import {Typography} from 'antd'
-const {Title}  = Typography
+import { Typography } from "antd";
+import Demo from "./Demo";
+const { Title } = Typography;
 class Editor extends React.Component {
-
-    render() {
-      return (
+  render() {
+    return (
+      <div>
         <Title>编辑页面</Title>
-      );
-    }
+        <Demo />
+      </div>
+    );
   }
+}
 
-  export default Editor;
+export default Editor;
