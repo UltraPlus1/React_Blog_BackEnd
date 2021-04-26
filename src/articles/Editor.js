52f591527f2121a284cb876bdd72e7bd439322fc
Mason <1076536974@qq.com>
2021-04-27 00:10:21 +0800

    改造react hooks 组件

--- src/articles/Editor.js
+++ src/articles/Editor.js
@@ -1,58 +1,50 @@
-import React from "react";
+import React,{useState} from "react";
 import SimpleMDEReact from "react-simplemde-editor";
-import { Typography } from "antd";
+import { Form, Input, Button, Typography } from 'antd';
 import hljs from 'highlight.js';
 import "easymde/dist/easymde.min.css";
 import 'highlight.js/styles/zenburn.css';
 import './Editor.css';
 import "font-awesome/css/font-awesome.min.css";
 const { Title } = Typography;
-class Editor extends React.Component {
-  state = {
-    textValue: "Start writing!!",
-  };
-
-  handleChange = (value) => {
-    this.setState({
-      textValue: value,
-    });
-  };
-
-  render() {
-    return (
-      <div className="container container-narrow">
-        <Title>编辑文章</Title>
-        <SimpleMDEReact
-          value={this.state.textValue}
-          onChange={this.handleChange}
-          options={{
-            autoDownloadFontAwesome:false,
-            scrollbarStyle: "native",
-            autofocus: true,
-            minHeight: "60vh",
-            hideIcons: [],
-            spellChecker:false,
-            renderingConfig: {
-              codeSyntaxHighlighting: true,
-              hljs:hljs,
-              markedOptions: {
-                gfm: true,
-                pedantic: false,
-                sanitize: false,
-                tables: true,
-                breaks: true,
-                smartLists: true,
-                smartypants: true,
-                highlight: function (code) {
-                        return hljs.highlightAuto(code).value;
-                }
+const Editor = () => {
+  const [textValue,setTextValue] = useState('Start writing!!')
+  return (
+    <div className="container container-narrow">
+      <Title>编辑文章</Title>
+      <Form
+      layout="vertical"
+      ></Form>
+      <SimpleMDEReact
+        value={textValue}
+        onChange={setTextValue}
+        options={{
+          autoDownloadFontAwesome:false,
+          scrollbarStyle: "native",
+          autofocus: true,
+          minHeight: "60vh",
+          hideIcons: [],
+          spellChecker:false,
+          renderingConfig: {
+            codeSyntaxHighlighting: true,
+            hljs:hljs,
+            markedOptions: {
+              gfm: true,
+              pedantic: false,
+              sanitize: false,
+              tables: true,
+              breaks: true,
+              smartLists: true,
+              smartypants: true,
+              highlight: function (code) {
+                      return hljs.highlightAuto(code).value;
               }
-            },
-          }}
-        />
-      </div>
-    );
-  }
+            }
+          },
+        }}
+      />
+    </div>
+  );
 }
 
 export default Editor;
