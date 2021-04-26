5ed8033b83e7587ca314a7c26ae7da7475676f3d
Mason <1076536974@qq.com>
2021-04-27 00:44:57 +0800

    更新按钮和部分样式

--- src/articles/Editor.js
+++ src/articles/Editor.js
@@ -6,15 +6,37 @@ import "easymde/dist/easymde.min.css";
 import 'highlight.js/styles/zenburn.css';
 import './Editor.css';
 import "font-awesome/css/font-awesome.min.css";
+import FormItem from "antd/lib/form/FormItem";
 const { Title } = Typography;
 const Editor = () => {
-  const [textValue,setTextValue] = useState('Start writing!!')
+  const [textValue,setTextValue] = useState('Start writing!!');
+  const [titleValue,setTitleValue] = useState('');
   return (
     <div className="container container-narrow">
-      <Title>编辑文章</Title>
-      <Form
-      layout="vertical"
-      ></Form>
+      <Title level={2}>编辑文章</Title>
+      <div className = "title-line">
+        <Form
+        layout="vertical"
+        >
+          <Form.Item 
+          name="title"
+          rules={[
+            {
+              required:true,
+              message:'请输入文章的标题'
+            }
+          ]}>
+            <Input 
+            value={titleValue}
+            onChange={(e)=>{setTitleValue(e.target.value)}}
+            placeholder="请输入文章标题"
+            style={{width:450}}
+            maxLength={50}
+            size="large"/>
+          </Form.Item>
+        </Form>
+        <Button type="primary" className="btn-position">发布文章</Button>
+      </div>
       <SimpleMDEReact
         value={textValue}
         onChange={setTextValue}
